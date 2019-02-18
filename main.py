2bbbe2ccfce834815d1220dee8386738d0bd5291
colidur <durand.coline.dc@gmail.com>
2019-02-18 16:39:33 +0100

    Pie charts

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import matplotlib.pyplot as plt
 
-years = [1950, 1955, 1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000,
-         2005, 2010, 2015]
+#PieChart
+labels = 'Python', 'C++', 'Ruby', 'PHP', 'Java', 'Perl'
+sizes = [33, 52, 12, 57, 62, 28]
+separated = (.2, .05, .05, .05, .05, .05)
 
-pops = [2.5, 2.7, 3.0, 3.3, 3.6, 4.0, 4.4, 4.8, 5.3, 5.7, 6.1,
-        6.5, 6.9, 7.3]
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', explode=separated)
+plt.axis('equal')
 
-deaths = [1.2, 1.7, 1.8, 2.2, 2.5, 2.7, 2.9,
-          3, 3.1, 3.3, 3.5, 3.8, 4.0, 4.3]
-
-plt.plot(years, pops, '--', color=(240/255, 140/255, 140/255))
-plt.plot(years, deaths, color=(140/255, 140/255, 230/255))
-
-plt.ylabel("Population in billions")
-plt.xlabel("Population growth by year")
-
-plt.title("Population growth")
 plt.show()
